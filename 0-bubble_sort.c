27247ec264577dd7b28b8ed882e5ba4dc79b1543
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-17 02:54:09 -0800

    Bubble Sort Fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,6 +8,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int temp;
 
 	if (!array || !size)
 		return;
@@ -18,7 +19,10 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
 				print_array(array, size);
 			}
 		}
